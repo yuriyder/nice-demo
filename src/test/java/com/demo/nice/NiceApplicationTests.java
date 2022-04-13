c121fa4df691067b373ccfdacb23e666010359c2
Yuriy Derevyanko <yuriy.derevyanko@nice.com>
2022-04-13 15:56:05 +0300

    Create skeleto spring-boot

---
+++ src/test/java/com/demo/nice/NiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.nice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
